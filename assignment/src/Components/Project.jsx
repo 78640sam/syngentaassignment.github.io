bc248e8d82647b13a472f5e04dca1692bbe26fc5
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-30 04:29:02 +0530

    updated

--- assignment/src/Components/Project.jsx
+++ assignment/src/Components/Project.jsx
@@ -1,6 +1,8 @@
 import "./style.css";
 import { useState } from "react";
 function Project() {
+
+
     const [col1, setCol1] = useState("#dddaf3");
     const [col2, setCol2] = useState("#ffbbab");
     const [col3, setCol3] = useState("#5443c2");
@@ -8,93 +10,81 @@ function Project() {
     const [col5, setCol5] = useState("#515493");
 
     const [val, setVal] = useState();
+
     const [hide1, setHide1] = useState(true);
     const [hide2, setHide2] = useState(true);
     const [hide3, setHide3] = useState(true);
     const [hide4, setHide4] = useState(true);
     const [hide5, setHide5] = useState(true);
 
+
     const randomcol1 = () => {
         const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-
         setCol1(randomColor);
 
     }
     const randomcol2 = () => {
         const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-
-
-        setCol2(randomColor);
+         setCol2(randomColor);
 
     }
     const randomcol3 = () => {
         const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-
-
         setCol3(randomColor);
     }
+
     const randomcol4 = () => {
         const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-
-
         setCol4(randomColor);
     }
+
     const randomcol5 = () => {
         const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-
-
         setCol5(randomColor);
     }
     const show1 = () => {
-
         setHide1(false)
-
     }
+
     const show11 = () => {
-
         setHide1(true)
-
     }
+
     const show2 = () => {
-
         setHide2(false)
-
     }
-    const show12 = () => {
 
-        setHide2(true)
+    const show12 = () => {
+     setHide2(true)
 
     }
     const show3 = () => {
-
-        setHide3(false)
+     setHide3(false)
 
     }
     const show13 = () => {
-
         setHide3(true)
 
     }
     const show4 = () => {
-
         setHide4(false)
 
     }
     const show14 = () => {
-
         setHide4(true)
 
     }
     const show5 = () => {
-
         setHide5(false)
 
     }
     const show15 = () => {
-
         setHide5(true)
 
     }
+
+
+    
     const soot = () => {
 
         if (val === "1") {
@@ -126,25 +116,33 @@ function Project() {
         <div className="project">
 
             <div className="main-div">
-                <div className="main-div-1">
-                    {
-                        hide1 ? null : <div style={{ backgroundColor: `${col1}` }} className="div-1" onClick={show11} ></div>
-                    }
-                    {
-                        hide2 ? null : <div style={{ backgroundColor: `${col2}` }} className="div-2" onClick={show12}></div>
-                    }
-                    {
-                        hide3 ? null : <div style={{ backgroundColor: `${col3}` }} className="div-3" onClick={show13}></div>
-                    }
-                    {
-                        hide4 ? null : <div style={{ backgroundColor: `${col4}` }} className="div-4" onClick={show14}></div>
-                    }
-                    {
-                        hide5 ? null : <div style={{ backgroundColor: `${col5}` }} className="div-5" onClick={show15}></div>
-                    }
+                <div>
+                    <div className="empty-div-1">
+                        <h5>empty div</h5>
+                    </div>
+                    <div className="main-div-1">
 
+                        <br />
+                        {
+                            hide1 ? null : <div style={{ backgroundColor: `${col1}` }} className="div-1" onClick={show11} ></div>
+                        }
+                        {
+                            hide2 ? null : <div style={{ backgroundColor: `${col2}` }} className="div-2" onClick={show12}></div>
+                        }
+                        {
+                            hide3 ? null : <div style={{ backgroundColor: `${col3}` }} className="div-3" onClick={show13}></div>
+                        }
+                        {
+                            hide4 ? null : <div style={{ backgroundColor: `${col4}` }} className="div-4" onClick={show14}></div>
+                        }
+                        {
+                            hide5 ? null : <div style={{ backgroundColor: `${col5}` }} className="div-5" onClick={show15}></div>
+                        }
+
+                    </div>
                 </div>
                 <div className="main-div-2">
+                    <h5>5 circles</h5>
                     {
                         hide1 ? <div style={{ backgroundColor: `${col1}` }} onClick={randomcol1} className="div-1"></div> : null
                     }
@@ -164,11 +162,11 @@ function Project() {
                 </div>
 
                 <div className="main-div-3">
-                    <input type="text" onChange={(el) => {
+                    <input className="input-box" type="text" onChange={(el) => {
                         setVal(el.target.value);
 
-                    }} />
-                    <button onClick={soot} >Soot</button>
+                    }} /><br />
+                    <button className="btn" onClick={soot} >Shoot</button>
 
                 </div>
             </div>
