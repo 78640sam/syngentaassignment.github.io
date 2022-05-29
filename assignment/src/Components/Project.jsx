61e10f76cda17bc97e25e6871fe624dfcac2b689
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-29 20:41:03 +0530

    updated

--- assignment/src/Components/Project.jsx
+++ assignment/src/Components/Project.jsx
@@ -1,7 +1,26 @@
+import "./style.css";
+
 function Project() {
     return (
       <div className="project">
     
+    <div className="main-div">
+        <div className="main-div-1">
+
+        </div>
+        <div className="main-div-2">
+            <div className="div-1"></div>
+            <div className="div-1"></div>
+            <div className="div-1"></div>
+            <div className="div-1"></div>
+            <div className="div-1"></div>
+        </div>
+
+        <div className="main-div-3">
+            <input type="text" />
+            <button>Soot</button>
+        </div>
+    </div>
       </div>
     );
   }
