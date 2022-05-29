540a7b022c521458d3ce94fc5652439ec3d39ef6
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-30 01:26:37 +0530

    updated

--- assignment/src/Components/Project.jsx
+++ assignment/src/Components/Project.jsx
@@ -1,6 +1,35 @@
 import "./style.css";
-
+import {useState} from "react";
 function Project() {
+
+    const [val, setVal] = useState();
+    const [hide, setHide] = useState(true);
+
+
+    const soot = () => {
+if(val==1){
+alert("1")
+//  show1()
+}else if(val==2){
+    alert("2")   
+}
+else if(val==3){
+    alert("3")   
+}
+else if(val==4){
+    alert("4")   
+}
+else if(val==5){
+    alert("5")   
+}else{
+    alert("No")   
+}
+
+const show1 = () => {
+  setHide(true)
+}
+
+    }
     return (
       <div className="project">
     
@@ -9,6 +38,9 @@ function Project() {
 
         </div>
         <div className="main-div-2">
+            {
+              hide?   <div className="div-1"></div>:null
+            }
             <div className="div-1"></div>
             <div className="div-1"></div>
             <div className="div-1"></div>
@@ -17,8 +49,11 @@ function Project() {
         </div>
 
         <div className="main-div-3">
-            <input type="text" />
-            <button>Soot</button>
+            <input type="text"  onChange={(el) => {
+            setVal(el.target.value);
+
+          }} />
+            <button onClick={soot} >Soot</button>
         </div>
     </div>
       </div>
